70419078b1d20eaea9e0e5b6272d684efd69cdc3
josephfrench <joe.p.french@gmail.com>
2022-02-08 10:07:06 -0500

    Update log-it.ts

--- log-it.ts
+++ log-it.ts
@@ -1,7 +1,8 @@
 /***
 * @param label
 * Pipeable operator to add within a .pipe() chain to console log the latest emission
-* Operationally takes a label that will be logged preceding the data
+* Optionally takes a label that will be logged preceding the data
+* Not recommended to leave in production code - convience method for debugging
 * Example usage: 
 *     myObservable.pipe(
 *        logIt()
