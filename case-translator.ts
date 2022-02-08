739461703689950c86a7406afe3f8adc01e99709
josephfrench <joe.p.french@gmail.com>
2022-02-08 12:16:58 -0500

    Create case-translator.ts

---
+++ case-translator.ts
@@ -0,0 +1,4 @@
+/***
+* https://stackblitz.com/edit/angular-api-translator?file=src/app/shared/services/api-translator.service.ts
+* https://tech.uqido.com/2020/02/27/snake-case-to-camel-case-bring-peace-between-rails-and-javascript/
+***/
